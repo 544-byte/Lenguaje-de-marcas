7c7944414837d4413d342877d09a104646644248
544-byte <544@cifpceuta.es>
2026-02-17 21:16:15 +0100

    cosas

---
+++ Tema5/Ejercicio1/script.js
@@ -0,0 +1,26 @@
+function crearTarjeta(user){
+    const tarjeta = document.createElement("div");
+    tarjeta.innerHTML = "<p>Nombre : " + user.name + "</p>"
+     + "<p>Nombre Usuario : " + user.username + "</p>"
+     + "<p>Email : " + user.email + "</p>"
+     + "<p>Dirección : </p>"
+     + "<ul> <li> Calle : " + user.address.street + "</li>"
+     + "<li> Suite : " + user.address.suite + "</li>"
+     + "<li> City : " + user.address.city + "</li>"
+     + "<li> Zipcode : " + user.address.zipcode + "</li>"
+     + "<li> Localización : <ul>"
+     + "<li> Latitud : " + user.address.geo.lat + "</li>"
+     + "<li> Longitud : " + user.address.geo.lng + "</li> </ul> </li> </ul>"
+     + "<p>tlf : " + user.phone + "</p>"
+     + "<p>website : " + user.website + "</p>"
+     + "<p>company : " + user.company.name + "</p>";
+
+    document.body.appendChild(tarjeta);
+}
+
+fetch("https://jsonplaceholder.typicode.com/users/1")
+.then(response => response.json())
+.then(user => {
+    crearTarjeta(user);
+
+});
